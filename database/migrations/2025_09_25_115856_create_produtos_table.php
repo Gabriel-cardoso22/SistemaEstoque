cf1ef821d882941b947f1bc73f1a06ec515952d9
Vitor Hugo Belorio Simão <vitorhugobsimao@gmail.com>
2025-09-25 09:08:58 -0300

    :sparkles:	feat: Criando as migrations para gerar as tabelas do banco de dados com base nas models.

---
+++ database/migrations/2025_09_25_115856_create_produtos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->decimal('preco', 10, 2);
+            $table->integer('quantidade')->default(0);
+            $table->string('categoria')->nullable();
+
+            // chave estrangeira para quem cadastrou (apenas gerente)
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
