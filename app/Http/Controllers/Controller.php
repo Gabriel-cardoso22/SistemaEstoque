5cb5f56e3c60cde78e00b91f702c10bbb99ab474
Gustavo Assis Coladello <gustavo.coladello@gmail.com>
2025-10-21 18:56:52 -0300

    Resolvendo conflitos no controller
    Adicionado classe ProdutoController, refereciando no web.php e ajustando Controller original

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,22 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Produto;
-
-class ProdutoController extends Controller
+abstract class Controller
 {
-    public function store(Request $request)
-    {
-        $request->validate([
-            'nome' => 'required|string|max:255',
-            'categoria' => 'required|string|max:255',
-            'preco' => 'required|numeric',
-            'quantidade' => 'required|integer',
-        ]);
-
-        $produto = Produto::create($request->all());
-
-        return response()->json($produto);
-    }
+    //
 }
