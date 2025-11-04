03b38e32a1cce81603e20b847d8cadf3bab707c6
Vitor Hugo Belorio Simão <vitorhugobsimao@gmail.com>
2025-11-04 16:05:50 -0300

    AB#98 ♻️refactor: Reorganizando e documentando (nomeando cada rota) de forma separada com sua respectiva  funcionalidade.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GerenteController;
-use App\Http\Controllers\ProdutoController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,22 +27,49 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 | Rotas Protegidas (somente autenticados)
 |--------------------------------------------------------------------------
 */
-
 Route::middleware(['auth'])->group(function () {
-    // Dashboard do gerente
-    Route::get('/dashboard/gerente', [GerenteController::class, 'index'])->name('dashboard.gerente');
-    // Lista de gerentes
-    Route::get('/gerentes/listar', [GerenteController::class, 'list'])
-        ->name('gerentes.list');
 
+    /*
+    |--------------------------------------------------------------------------
+    | ROTAS GERENTE
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('gerente')->group(function () {
+        // Dashboard do gerente (rota usada no AuthController)
+        Route::get('/dashboard', [GerenteController::class, 'index'])->name('dashboard.gerente');
 
-    // Dashboard do funcionário
-    Route::get('/dashboard/funcionario', [FuncionarioController::class, 'funcionario'])->name('dashboard.funcionario');
+        // CRUD de gerentes
+        Route::get('/', [GerenteController::class, 'list'])->name('gerentes.index');
+        Route::get('/create', [GerenteController::class, 'create'])->name('gerentes.create');
+        Route::post('/', [GerenteController::class, 'store'])->name('gerentes.store');
+        Route::get('/{gerente}/edit', [GerenteController::class, 'edit'])->name('gerentes.edit');
+        Route::put('/{gerente}', [GerenteController::class, 'update'])->name('gerentes.update');
+        Route::delete('/{gerente}', [GerenteController::class, 'destroy'])->name('gerentes.destroy');
+    });
 
-    // CRUD de gerentes
-    Route::resource('gerentes', GerenteController::class)->except(['index']);
+    /*
+    |--------------------------------------------------------------------------
+    | ROTAS FUNCIONÁRIO
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('funcionario')->group(function () {
+        // Dashboard do funcionário (rota usada no AuthController)
+        Route::get('/dashboard', [FuncionarioController::class, 'index'])->name('dashboard.funcionario');
 
-    // CRUD de produtos
+        // CRUD de funcionários
+        Route::get('/', [FuncionarioController::class, 'index'])->name('funcionarios.index');
+        Route::get('/create', [FuncionarioController::class, 'create'])->name('funcionarios.create');
+        Route::post('/', [FuncionarioController::class, 'store'])->name('funcionarios.store');
+        Route::get('/{funcionario}/edit', [FuncionarioController::class, 'edit'])->name('funcionarios.edit');
+        Route::put('/{funcionario}', [FuncionarioController::class, 'update'])->name('funcionarios.update');
+        Route::delete('/{funcionario}', [FuncionarioController::class, 'destroy'])->name('funcionarios.destroy');
+    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | ROTAS PRODUTOS
+    |--------------------------------------------------------------------------
+    */
     Route::resource('produtos', ProdutoController::class);
 });
 
